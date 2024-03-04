efc60d05257ed5118723c30cf74816e49a4c784d
mohammadrezaGh <mohammadreza.gharghabi6@gmail.com>
2024-03-04 16:07:48 +0330

    add requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 
 # Media config
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
